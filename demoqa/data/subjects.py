22b74d3f482563b60e6a2f64b154c7836642da5a
Grigoriy Amelin <gamelin@ecom.tech>
2026-02-09 23:32:32 +0300

    Crated Users and Subjects classes Updated conftest.py

---
+++ demoqa/data/subjects.py
@@ -0,0 +1,8 @@
+class Subject:
+    def __init__(self, keys, subject):
+        self.keys = keys
+        self.subject = subject
+
+subject_english = Subject('e', 'English')
+subject_commerce = Subject('erc', 'Commerce')
+subject_arts = Subject('a', 'Arts')
---
+++ demoqa/data/users.py
@@ -0,0 +1,42 @@
+import subjects
+
+from dataclasses import dataclass
+
+from demoqa.data.subjects import Subject
+
+
+
+@dataclass
+class Users:
+    first_name: str
+    last_name: str
+    email: str
+    gender: str
+    phone: str
+    birth_day: str
+    birth_month: str
+    birth_year: str
+    subject: list[Subject] | Subject
+    hobby: str
+    file_name: str
+    address: str
+    state: str
+    city: str
+
+
+user = Users(
+    first_name='Mirko',
+    last_name='Stojanovic',
+    email='mirko-stojanovic@srbijavoz.rs',
+    gender='Male',
+    phone='0682826586',
+    birth_day='08',
+    birth_month='October',
+    birth_year='1979',
+    hobby='Reading',
+    subject=[subjects.subject_english, subjects.subject_commerce, subjects.subject_arts],
+    file_name='111.png',
+    address='11545 Novi Sad, Srbija, ul. Cara Dusana, 9, sprat 2, stan 231',
+    state='Haryana',
+    city='Panipat'
+)
