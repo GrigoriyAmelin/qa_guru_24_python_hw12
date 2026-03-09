1f1d3e4685dbcd4e32696af30501c647b7840a45
Grigoriy Amelin <gamelin@ecom.tech>
2026-03-09 23:21:48 +0300

    Added remote driver executor on selenoid

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,11 +2,14 @@ import allure_commons
 import pytest
 from selene import browser, support
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture(scope='function', autouse=True)
 def browser_settings():
-    browser.config.driver_name = 'edge'
+    options = Options()
+
+    browser.config.driver_name = 'chrome'
     browser.config.window_width = 1920
     browser.config.window_height = 1080
     browser.config.base_url = 'https://demoqa.com'
@@ -15,10 +18,17 @@ def browser_settings():
     driver_options.page_load_strategy = 'eager'
     browser.config.driver_options = driver_options
 
+    driver = webdriver.Remote(
+        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
+
+    browser.config.driver = driver
+
     browser.config._wait_decorator = support._logging.wait_with(
         context=allure_commons._allure.StepContext
     )
 
-    yield
+    yield driver
 
-    browser.quit()
+    driver.quit()
